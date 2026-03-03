e905aa437fcdaa07f16e65eabf19f5e365a9536e
bsfluh <bsfluh@gmail.com>
2026-03-03 18:34:18 +0300

    update table db

--- migrations/20260227142210_create_users.sql
+++ migrations/20260227142210_create_users.sql
@@ -4,7 +4,7 @@ CREATE TABLE users(
     name TEXT NOT NULL,
     email TEXT NOT NULL UNIQUE,
     password_hash TEXT NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
 -- +goose Down
